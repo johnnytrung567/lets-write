f09d294b6e70a6b0afb6aaa0707ec077e54373c6
johnnytrung567 <vantrung242628@gmail.com>
2022-09-01 15:30:21 +0700

    Set expire time for token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -51,7 +51,8 @@ router.post('/register', async (req, res) => {
         // Return access token
         const token = jwt.sign(
             { userId: newUser._id },
-            process.env.ACCESS_TOKEN_SECRET
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '7d' }
         )
 
         res.json({
@@ -82,28 +83,25 @@ router.post('/login', async (req, res) => {
         // Check for existing user
         const user = await User.findOne({ email })
         if (!user)
-            return res
-                .status(400)
-                .json({
-                    success: false,
-                    message: 'Incorrect email or password',
-                })
+            return res.status(400).json({
+                success: false,
+                message: 'Incorrect email or password',
+            })
 
         // Check valid password
         const passwordValid = await argon2.verify(user.password, password)
         if (!passwordValid)
-            return res
-                .status(400)
-                .json({
-                    success: false,
-                    message: 'Incorrect email or password',
-                })
+            return res.status(400).json({
+                success: false,
+                message: 'Incorrect email or password',
+            })
 
         // All good
         // Return access token
         const token = jwt.sign(
             { userId: user._id },
-            process.env.ACCESS_TOKEN_SECRET
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '7d' }
         )
 
         res.json({
