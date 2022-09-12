a1ad2d22bc167028de992b4793800615d7b5a23d
johnnytrung567 <vantrung242628@gmail.com>
2022-09-12 16:09:30 +0700

    Fix Category model import

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const verifyToken = require('../middleware/verifyToken')
-const Category = require('../models/category')
+const Category = require('../models/Category')
 
 // Get all categories
 router.get('/', async (req, res) => {
